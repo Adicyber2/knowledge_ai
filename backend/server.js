ab9c85278aa349373d557238977124fe6079cf65
Adicyber2 <adityadeshmukh1404@gmail.com>
2026-09-07 17:53:40 +0530

    cors change

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://knowledge-backend-zjuz.onrender.com",
+    "https://knowledge-frontedn.onrender.com",
     "chrome-extension://",
   ],
   credentials: true,
